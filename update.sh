705b58ca13e1c28838b9ed4696028fbc8908f761
ErrorNoInternet <error.nointernet@gmail.com>
2023-05-21 11:57:22 +0800

    Small changes

--- update.sh
+++ update.sh
@@ -3,8 +3,6 @@
 modified=false
 
 for file in $(find . -type f -name "*.spec"); do
-	version=$(cat $file | grep "Version: " | cut -d' ' -f2)
-
 	url=$(cat $file | grep "URL: " | cut -d' ' -f2)
 	repository=$(echo $url | sed -n 's|.*github.com/\(.*\)$|\1|p')
 	api_response=$(curl -s "https://api.github.com/repos/$repository/releases/latest")
@@ -12,7 +10,9 @@ for file in $(find . -type f -name "*.spec"); do
 		echo failed to request latest version for $repository!
 		continue
 	fi
+
 	latest_version=$(echo "$api_response" | grep --color=never tag_name | sed -n 's|.*".*": "\(.*\)".*|\1|p' | sed 's|^v||')
+	version=$(cat $file | grep "Version: " | cut -d' ' -f2)
 
 	if [[ "$version" != "$latest_version" ]]; then
 		echo "$file is not up-to-date ($version -> $latest_version)"
@@ -22,13 +22,13 @@ for file in $(find . -type f -name "*.spec"); do
 		echo "$updated_file" > $file
 
 		echo "running git add && git commit..."
-		echo "=========="
+		echo ">>>>>>>>>>"
 		git add $file
 		git commit -m "Update $repository"
-		echo "=========="
+		echo "<<<<<<<<<<"
 
 		modified=true
-		echo "successfully updated $file from $version to $latest_version!"
+		echo "successfully updated $file ($version -> $latest_version)"
 	else
 		echo "$file is up-to-date ($latest_version)"
 	fi
@@ -36,7 +36,7 @@ done
 
 if [[ $modified = true ]]; then
 	echo "running git push..."
-	echo "=========="
+	echo ">>>>>>>>>>"
 	git push
-	echo "=========="
+	echo "<<<<<<<<<<"
 fi
