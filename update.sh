b25c928272d4fc5f2b6f8953e2a012a5a4c9fc6b
ErrorNoInternet <errornointernet@envs.net>
2023-08-16 14:15:15 +0800

    Tabs -> Spaces

--- update.sh
+++ update.sh
@@ -15,49 +15,49 @@ for file in $(find . -type f -name "*.spec"); do
         continue
     fi
 
-	url=$(cat $file | grep "URL: " | cut -d' ' -f2)
-	repository=$(echo $url | sed -n 's|.*github.com/\(.*\)$|\1|p')
-	api_response=$(curl -s "https://api.github.com/repos/$repository/releases/latest")
-	if [[ $? -ne 0 ]]; then
-		echo "failed to request latest version for $repository!"
-		echo -e "api response:\n$api_response"
-		continue
-	fi
+    url=$(cat $file | grep "URL: " | cut -d' ' -f2)
+    repository=$(echo $url | sed -n 's|.*github.com/\(.*\)$|\1|p')
+    api_response=$(curl -s "https://api.github.com/repos/$repository/releases/latest")
+    if [[ $? -ne 0 ]]; then
+        echo "failed to request latest version for $repository!"
+        echo -e "api response:\n$api_response"
+        continue
+    fi
 
-	latest_version=$(echo "$api_response" | grep --color=never tag_name | sed -n 's|.*".*": "\(.*\)".*|\1|p' | sed 's|^v||')
-	if [[ -z "$latest_version" ]]; then
-		echo "failed to request latest version for $repository!"
-		echo -e "api response:\n$api_response"
-		continue
-	fi
-	version=$(cat $file | grep "Version: " | cut -d' ' -f2)
+    latest_version=$(echo "$api_response" | grep --color=never tag_name | sed -n 's|.*".*": "\(.*\)".*|\1|p' | sed 's|^v||')
+    if [[ -z "$latest_version" ]]; then
+        echo "failed to request latest version for $repository!"
+        echo -e "api response:\n$api_response"
+        continue
+    fi
+    version=$(cat $file | grep "Version: " | cut -d' ' -f2)
 
-	if [[ "$version" != "$latest_version" ]]; then
-		echo "$file is not up-to-date ($version -> $latest_version)"
+    if [[ "$version" != "$latest_version" ]]; then
+        echo "$file is not up-to-date ($version -> $latest_version)"
 
-		updated_file=$(cat $file)
-		echo "modifying version in file..."
-		updated_file=$(echo "$updated_file" | sed "s|Version: $version|Version: $latest_version|")
-		echo "modifying release in file..."
-		updated_file=$(echo "$updated_file" | sed "s|Release: [0-9]\+%{?dist}|Release: 1%{?dist}|")
-		echo "$updated_file" > $file
+        updated_file=$(cat $file)
+        echo "modifying version in file..."
+        updated_file=$(echo "$updated_file" | sed "s|Version: $version|Version: $latest_version|")
+        echo "modifying release in file..."
+        updated_file=$(echo "$updated_file" | sed "s|Release: [0-9]\+%{?dist}|Release: 1%{?dist}|")
+        echo "$updated_file" > $file
 
-		echo "running git add && git commit..."
-		echo ">>>>>>>>>>"
-		git add $file
-		git commit -m "Update $repository"
-		echo "<<<<<<<<<<"
+        echo "running git add && git commit..."
+        echo ">>>>>>>>>>"
+        git add $file
+        git commit -m "Update $repository"
+        echo "<<<<<<<<<<"
 
-		modified=true
-		echo "successfully updated $file ($version -> $latest_version)"
-	else
-		echo "$file is up-to-date ($latest_version)"
-	fi
+        modified=true
+        echo "successfully updated $file ($version -> $latest_version)"
+    else
+        echo "$file is up-to-date ($latest_version)"
+    fi
 done
 
 if [[ $modified = true ]]; then
-	echo "running git push..."
-	echo ">>>>>>>>>>"
-	git push
-	echo "<<<<<<<<<<"
+    echo "running git push..."
+    echo ">>>>>>>>>>"
+    git push
+    echo "<<<<<<<<<<"
 fi
