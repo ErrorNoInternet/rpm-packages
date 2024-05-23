3e791b832256fd1bd821388a46ee46d739468308
ErrorNoInternet <errornointernet@envs.net>
2024-05-23 22:39:32 +0800

    build.sh: try using fedpkg srpm

--- build.sh
+++ build.sh
@@ -2,11 +2,14 @@
 
 set -x
 
+dnf install -y fedpkg
+
 sed -i "s|%{?dist}|.patched%{?dist}|" "$spec"
 sed -i "s|%autorelease|%autorelease -e patched|" "$spec"
 sed -i "s|-S git_am|-S git|" "$spec"
+
 patch -p1 < ../downstream.patch
 
 cp -r ./* /builddir/build/SOURCES
-rpmbuild -bs "$spec"
-cp -r /builddir/build/SRPMS/* "$outdir"
+fedpkg srpm
+cp -r ./*.src.rpm "$outdir"
