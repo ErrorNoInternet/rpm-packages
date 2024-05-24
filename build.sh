8a2a26a4ef6e0d5d15a41e0c57e13178705f7ff0
ErrorNoInternet <errornointernet@envs.net>
2024-05-24 14:36:55 +0800

    build.sh: do not match %%{?dist}

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ dnf install -y git fedpkg
 git config --global --add safe.directory "$(pwd)"
 
 git checkout f40
-sed -i "s|%{?dist}|.patched%{?dist}|" "$spec"
+sed -i "s|\([^%]\)%{?dist}|\1.patched%{?dist}|" "$spec"
 sed -i "s|%autorelease|%autorelease -e patched|" "$spec"
 sed -i "s|-S git_am|-S git|" "$spec"
 
