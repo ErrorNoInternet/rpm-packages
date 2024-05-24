4b28b47d9e716b0b60678f88defddc7014c11f4f
ErrorNoInternet <errornointernet@envs.net>
2024-05-24 14:18:21 +0800

    treewide: checkout branch manually

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 
 set -x
 
-dnf install -y fedpkg
+dnf install -y git fedpkg
 
+git checkout f40
 sed -i "s|%{?dist}|.patched%{?dist}|" "$spec"
 sed -i "s|%autorelease|%autorelease -e patched|" "$spec"
 sed -i "s|-S git_am|-S git|" "$spec"
 
 patch -p1 < ../downstream.patch
 
-cp -r ./* /builddir/build/SOURCES
 fedpkg srpm
 cp -r ./*.src.rpm "$outdir"
