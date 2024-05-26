695617adfc11a2a0ce40148df8ed8f88dc9a2e9e
ErrorNoInternet <errornointernet@envs.net>
2024-05-26 19:45:52 +0800

    build.sh: refactor

--- build.sh
+++ build.sh
@@ -3,16 +3,13 @@
 set -x
 
 dnf install -y git fedpkg
-git config --global --add safe.directory "$(pwd)"
-
+git config --global --add safe.directory "$PWD"
 git checkout f40
-echo "%global _default_patch_fuzz 2" > "$spec".bak
-cat "$spec" >> "$spec".bak
-cat "$spec".bak > "$spec"
+
+printf "%s\n%s\n" "%global _default_patch_fuzz 2" "$(cat "$spec")" > "$spec"
 sed -i "s|\([^%]\)%{?dist}|\1.patched%{?dist}|" "$spec"
 sed -i "s|%autorelease|%autorelease -e patched|" "$spec"
 sed -i "s|-S git_am|-S git|" "$spec"
-
 patch -p1 < ../downstream.diff
 
 fedpkg srpm
