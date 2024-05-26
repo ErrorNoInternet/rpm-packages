4007ee6cf78db12861994ad029397d1369c72b0d
ErrorNoInternet <errornointernet@envs.net>
2024-05-26 14:00:47 +0800

    treewide: set patch fuzz in build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ dnf install -y git fedpkg
 git config --global --add safe.directory "$(pwd)"
 
 git checkout f40
+echo "%global _default_patch_fuzz 2" > "$spec".bak
+cat "$spec" >> "$spec".bak
+cat "$spec".bak > "$spec"
 sed -i "s|\([^%]\)%{?dist}|\1.patched%{?dist}|" "$spec"
 sed -i "s|%autorelease|%autorelease -e patched|" "$spec"
 sed -i "s|-S git_am|-S git|" "$spec"
