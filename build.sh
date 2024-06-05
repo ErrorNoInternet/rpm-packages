a9a81278c5288a70fd64e4ea2ff9032257490018
ErrorNoInternet <errornointernet@envs.net>
2024-06-05 16:31:13 +0800

    build.sh: change patch order

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ dnf install -y git fedpkg
 git config --global --add safe.directory "$PWD"
 git checkout f40
 
+patch -p1 < ../downstream.diff
 printf "%s\n%s\n" "%global _default_patch_fuzz 2" "$(cat "$spec")" > "$spec"
 sed -i "s|\([^%]\)%{?dist}|\1.patched%{?dist}|" "$spec"
 sed -i "s|%autorelease|%autorelease -e patched|" "$spec"
 sed -i "s|-S git_am|-S git|" "$spec"
-patch -p1 < ../downstream.diff
 
 fedpkg srpm
 cp -r ./*.src.rpm "$outdir"
