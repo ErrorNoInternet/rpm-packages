11686cea0a508acbf5052611563d6088e1335ac9
ErrorNoInternet <errornointernet@envs.net>
2024-06-22 16:17:14 +0800

    rofi-emoji: init

--- update.sh
+++ update.sh
@@ -17,6 +17,7 @@ declare -A package_ids=(
 	["par2cmdline-turbo/par2cmdline-turbo.spec"]=372791
 	["prismlauncher/prismlauncher.spec"]=301949
 	["ripdrag/ripdrag.spec"]=372793
+	["rofi-emoji/rofi-emoji.spec"]=242096
 	["satty/satty.spec"]=372795
 	["swaylock-effects/swaylock-effects.spec"]=312399
 	["swaync/swaync.spec"]=242061
