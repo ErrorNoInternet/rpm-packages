5ea486b63b2f4237d061e2a1d2194535faa36792
ErrorNoInternet <errornointernet@envs.net>
2024-07-22 14:31:33 +0800

    build.sh: handle failing downstream diffs

--- build.sh
+++ build.sh
@@ -6,8 +6,11 @@ dnf install -y git fedpkg
 git config --global --add safe.directory "$PWD"
 git checkout f40
 
-patch -p1 < ../downstream.diff
-printf "%s\n%s\n" "%global _default_patch_fuzz 2" "$(cat "$spec")" > "$spec"
+patch -p1 <../downstream.diff || {
+	echo "downstream diff failed to apply"
+	exit 1
+}
+printf "%s\n%s\n" "%global _default_patch_fuzz 2" "$(cat "$spec")" >"$spec"
 sed -i "s|\([^%]\)%{?dist}|\1.patched%{?dist}|" "$spec"
 sed -i "s|%autorelease|%autorelease -e patched|" "$spec"
 sed -i "s|-S git_am|-S git|" "$spec"
