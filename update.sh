c3f215cce09e51afe81e3fc1157c49f25cd0c7c8
ErrorNoInternet <errornointernet@envs.net>
2024-11-01 12:58:48 -0400

    update.sh: remove onefetch

--- update.sh
+++ update.sh
@@ -16,7 +16,6 @@ declare -A anitya_ids=(
 	["kwin-effects/kwin-effects-sliding-notifications.spec"]=372805
 	["lowfi/lowfi.spec"]=374761
 	["mergerfs/mergerfs.spec"]=372789
-	["onefetch/onefetch.spec"]=141703
 	["par2cmdline-turbo/par2cmdline-turbo.spec"]=372791
 	["prismlauncher/prismlauncher.spec"]=301949
 	["ripdrag/ripdrag.spec"]=372793
